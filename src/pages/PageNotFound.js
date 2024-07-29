9f09b78333c45b2491d78bf366d2432aa2fa61f4
kimyeonji713 <hfgj0014@naver.com>
2024-07-29 20:45:47 +0900

    404완성

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,3 +1,35 @@
+import { TbMoodSadDizzy } from "react-icons/tb";
+import styled from "styled-components";
+import { colors } from "../GlobalStyled";
+import { Link } from "react-router-dom";
+import { routes } from "../routes";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  font-size: 300px;
+  color: #555;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  h3 {
+    font-size: 100px;
+  }
+  a {
+    margin-top: 50px;
+    font-size: 16px;
+    text-decoration: underline;
+    color: #777;
+  }
+`;
+
 export const PageNotFound = () => {
-  return <div>404</div>;
+  return (
+    <Container>
+      <TbMoodSadDizzy />
+      <h3>404</h3>
+      <Link to={routes.home}>메인으로 돌아가기</Link>
+    </Container>
+  );
 };
