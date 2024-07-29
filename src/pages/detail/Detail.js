4115e97881b8490baf99c38a019b8f61765a5cec
kimyeonji <hfgj0014@naver.com>
2024-07-29 17:38:25 +0900

    디테일 중간저장

--- src/pages/detail/Detail.js
+++ src/pages/detail/Detail.js
@@ -2,10 +2,18 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import { movieDetail } from "../../api";
+import { ORIGIN_URL, W500_URL } from "../../constant/imgUrl";
+import { Loading } from "../../components/Loading";
 
-const Container = styled.div``;
-const CoverImg = styled.div``;
+const Container = styled.div`
+  padding: 100px;
+  width: 100%;
+`;
+const CoverImg = styled.img``;
 const ConWrap = styled.div``;
+const Info = styled.div``;
+const Genres = styled.div``;
+const Desc = styled.div``;
 
 export const Detail = () => {
   const [isLoding, setIsLoading] = useState(false);
@@ -27,11 +35,37 @@ export const Detail = () => {
   }, [movieId]);
 
   return (
-    <Container>
-      <CoverImg />
-      <ConWrap>
-        <h3>타이틀</h3>
-      </ConWrap>
-    </Container>
+    <>
+      {isLoding ? (
+        <Loading />
+      ) : (
+        <Container>
+          <CoverImg
+            src={ORIGIN_URL + movieData?.backdrop_path}
+            alt={movieData?.title}
+          />
+          <ConWrap>
+            <h3>{movieData?.title}</h3>
+          </ConWrap>
+
+          <Info>
+            <span>{movieData?.release_date}</span>
+            <span>{movieData?.runtime}분</span>
+            <span>{Math.round(movieData?.vote_average)}점</span>
+          </Info>
+
+          <Genres>
+            {movieData?.genres.map((genre) => (
+              <li key={genre?.id}>{genre?.name}</li>
+            ))}
+          </Genres>
+
+          <Desc>
+            <h3>줄거리</h3>
+            <p>{movieData?.overview}</p>
+          </Desc>
+        </Container>
+      )}
+    </>
   );
 };
